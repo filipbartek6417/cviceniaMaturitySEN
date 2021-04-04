1877e14c0f92064ed8d5a28f9551ea6d9ba335ee
Filip <66736221+filipbartek6417@users.noreply.github.com>
2021-04-04 09:06:40 +0200

    zastavky - rolovanie textu

--- zastavky.py
+++ zastavky.py
@@ -1,4 +1,4 @@
-from tkinter import Canvas, Tk
+from tkinter import *
 
 main = Tk()
 
@@ -7,17 +7,29 @@ c.pack()
 
 with open('zastavky.txt') as f:
     zastavky = [line.rstrip() for line in f]
+
+zastavky[-1] += '   konecna zastavka'
 count = 0
 dlzka = len(zastavky)
+fps = 1000
 
 
 def show(event):
-    global c, count
+    global c, count, text
     if count < dlzka:
         c.delete('all')
-        c.create_text(500, 150, text=zastavky[count], font=('Arial', 100), fill='red')
+        text = c.create_text(1000, 150, text=zastavky[count], font=('Arial', 100), fill='red', anchor="w", tags="text")
         count += 1
+        move()
 
+def move():
+    global fps
+    x1, y1, x2, y2 = c.bbox("text")
+    if x2 < 0:
+        c.coords("text", 1000, 150)
+    else:
+        c.move("text", -2, 0)
+    c.after(50, move)
 
 c.bind('<Button-1>', show)
 main.mainloop()
